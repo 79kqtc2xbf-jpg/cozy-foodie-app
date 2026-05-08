81475582e149c342d0b6b2b50f97808bb6a266d0
79kqtc2xbf-jpg <79kqtc2xbf@privaterelay.appleid.com>
2026-05-09 04:27:55 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-const CACHE='cozy-foodie-v1';
-const FILES=['./','index.html','styles.css','app.js','recipes.json','manifest.json'];
-self.addEventListener('install', e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch', e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
