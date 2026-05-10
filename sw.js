69b7986c3976a34184ea601321de36e13c409205
Lisa <79kqtc2xbf@privaterelay.appleid.com>
2026-05-10 17:33:36 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', event => event.respondWith(fetch(event.request)));
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request));});
