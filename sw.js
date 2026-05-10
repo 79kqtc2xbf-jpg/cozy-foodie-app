a35b094f1f6fd3cc1eb49600a4f4c80a0ff143b1
Lisa <79kqtc2xbf@privaterelay.appleid.com>
2026-05-10 18:31:38 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request));});
+const CACHE = "cozy-foodie-v8";
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
